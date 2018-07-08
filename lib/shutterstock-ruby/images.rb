f84468817923967b3d109dc6ff0f08e97ad4b37b
ingojoseph <ingo.joseph@gmail.com>
2018-07-08 17:05:16 +0200

    add reverse image search

--- lib/shutterstock-ruby/images.rb
+++ lib/shutterstock-ruby/images.rb
@@ -6,5 +6,15 @@ module ShutterstockRuby
     def self.search(query, options = {})
       JSON.parse(self.get('/images/search', { query: query }.merge(options)))
     end
+
+    # to use similar you need to create an image first
+    def self.create(base64_image, options = {})
+      JSON.parse(self.post('/images', { base64_image: base64_image }, options))
+    end
+
+    def self.similar(id, options = {})
+      JSON.parse(self.get("/images/#{id}/similar", options))
+    end
+
   end
 end
